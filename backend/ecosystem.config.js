338316b836663fe22b886c55263a102833ed6c78
Delondthedark <delondtools@gmail.com>
2025-08-28 11:17:16 +0900

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,21 +1,19 @@
 // backend/ecosystem.config.js
-module.exports = {
-  apps: [
+export const apps = [
     {
-      name: 'backend',
-      script: 'index.js',
-      cwd: '/home/ubuntu/Pupil_App/backend',
-      env: {
-        PGUSER: 'pupil_user',
-        PGPASSWORD: 'pupil_pass',
-        PGHOST: 'localhost',
-        PGDATABASE: 'pupil_app_db',
-        PGPORT: '5432',
-        PORT: '3001',
-        PUBLIC_URL: 'https://hey.pupil.best',
-        JWT_SECRET: 'supersecret',
-        SHARED_UPLOAD_TOKEN: 'ipass'
-      }
+        name: 'backend',
+        script: 'index.js',
+        cwd: '/home/ubuntu/Pupil_App/backend',
+        env: {
+            PGUSER: 'pupil_user',
+            PGPASSWORD: 'pupil_pass',
+            PGHOST: 'localhost',
+            PGDATABASE: 'pupil_app_db',
+            PGPORT: '5432',
+            PORT: '3001',
+            PUBLIC_URL: 'https://hey.pupil.best',
+            JWT_SECRET: 'supersecret',
+            SHARED_UPLOAD_TOKEN: 'ipass'
+        }
     }
-  ]
-};
+];
